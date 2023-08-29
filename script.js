5f02b868188b880326bcdbb797a2d5a47dde8795
qasimjan1 <110812098+qasimjan1@users.noreply.github.com>
2023-08-29 16:29:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const toggleBtn = document.querySelector('.toggle-btn')
+const dropDwonMenu = document.querySelector('.drop-down-menu')
+toggleBtn.onclick = function (){
+    dropDwonMenu.classList.toggle('open')
+}
+
+
+// ************** on click for assessment page *********************888<script>
+  function moveToAnotherPage() {
+    window.location.href = "assessment.html";
+  }
+
+
+  // ************** on click for result page *********************<script>
+  function moveToAnotherPage() {
+    window.location.href = "result.html";
+  }
